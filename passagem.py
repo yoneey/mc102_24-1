3b09d53c2be38199e5a360f82e58ac23fc282162
yoneey <yago.157.e.y@gmail.com>
2024-05-10 21:38:57 -0300

    Iniciado a lista 8

--- passagem.py
+++ passagem.py
@@ -1,4 +1,4 @@
-# RA 210000
+# RA 222807 Yago Eizo Yonemura
 
 # Se necessario importe suas bibliotecas aqui
 
@@ -7,6 +7,10 @@ class Data:
     
     # Aqui em baixo ficam as variaveis de Data
     # Nao se esqueca de proteger as variaveis com @property e @field.setter
+    def __init__(self, dia, mes, ano):
+        self.dia = dia
+        self.mes = mes
+        self.ano = ano
     # .
     # .
     # .
@@ -15,12 +19,19 @@ class Data:
     # .
     # .
     # .
+    pass
 
 
 class Voo:
 
     # Aqui em baixo ficam as variaveis de Voo
     # Nao se esqueca de proteger as variaveis com @property e @field.setter
+    def __init__(self, codigo, origem, destino, data, valor):
+        self.codigo = codigo
+        self.origem = origem
+        self.destino = destino
+        self.data = data
+        self.valor = valor
     # .
     # .
     # .
@@ -29,6 +40,7 @@ class Voo:
     # .
     # .
     # .
+    pass
 
 
 # Aqui em baixo fica a sua solucao

8019bf7ec5c4fb932ac38ac5ec099fdb40e224f4
Yago Yonemura <Yago.157.e.y@gmail.com>
2024-05-16 19:04:15 -0300

    Melhorado

--- passagem.py
+++ passagem.py
@@ -1,5 +1,7 @@
 # RA 222807 Yago Eizo Yonemura
-
+#TODO como fazer os calculos com as datas?
+#TODO como fazer o cancelar voo
+#TODO fazer o algoritmo final
 # Se necessario importe suas bibliotecas aqui
 
 
@@ -8,9 +10,9 @@ class Data:
     # Aqui em baixo ficam as variaveis de Data
     # Nao se esqueca de proteger as variaveis com @property e @field.setter
     def __init__(self, dia, mes, ano):
-        self.dia = dia
-        self.mes = mes
-        self.ano = ano
+        self._dia = dia
+        self._mes = mes
+        self._ano = ano
     # .
     # .
     # .
@@ -19,6 +21,16 @@ class Data:
     # .
     # .
     # .
+    @property
+    def data(self):
+        return self._dia+"/"+self._mes+"/"+self._ano
+    
+    @data.setter
+    def data(self, dia, mes, ano):
+        self._dia = dia
+        self._mes = mes
+        self._ano = ano
+
     pass
 
 
@@ -31,7 +43,7 @@ class Voo:
         self.origem = origem
         self.destino = destino
         self.data = data
-        self.valor = valor
+        self._valor = valor
     # .
     # .
     # .
@@ -40,7 +52,83 @@ class Voo:
     # .
     # .
     # .
-    pass
+    @property
+    def codigo(self):
+        return self._codigo
+    
+    @property
+    def caminho(self):
+        return self._origem+" "+self._destino 
+    
+    @property
+    def data(self):
+        return self._data 
+    
+    @property
+    def valor(self):
+        return self._valor
+    
+    @codigo.setter
+    def codigo(self, codigo, data, valor):
+        self._codigo = codigo
+    
+    @caminho.setter    
+    def caminho(self, origem, destino):    
+        self._origem = origem
+        self._destino = destino
+    
+    @data.setter
+    def data(self, data):    
+        self._data = data
+    
+    @valor.setter    
+    def valor(self, valor):
+        self._valor = valor
+    
 
 
 # Aqui em baixo fica a sua solucao
+dicionario = {}
+
+for i in range():
+    operacao = input()
+    match operacao:
+        
+        case "registrar":
+            #Pega os atributos do voo
+            codigo = int(input())
+            origem, destino = input().split()
+            data = Data(map(int, input().split("/")))
+            valor = float(input())
+            
+            #Registra o voo como objeto
+            v = Voo(
+             codigo = codigo,
+             origem = origem,
+             destino = destino,
+             data = data,
+             valor = valor    
+            )
+            
+            #Adiciona o voo para o dicionario
+            dicionario[codigo] = v
+            
+        case "alterar":
+            #pega o codigo e valor novo
+            codigo, novo = input().split
+            
+            #define o valor antigo para o print
+            antigo = dicionario[codigo].valor
+            
+            #redefine o valor do voo
+            dicionario[codigo].valor = novo
+            print(f"{codigo} valor alterado de {antigo} para {novo}")
+            
+        case "cancelar":
+            #usar pop
+            pass
+        
+        case "planejar":
+            pass
+
+    
